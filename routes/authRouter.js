493820cdd31e51de2e002bd0f45da020f45ad1ce
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-04-20 19:07:18 +0300

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,6 +6,9 @@ const authRouter = express.Router();
 
 // sing up
 authRouter.post('/register', authController.singup);
+
+// sing in
+authRouter.post('/login', authController.signin);
 authRouter.get('/verify/:verificationToken', authController.verify);
 authRouter.post('/verify', authController.resendEmail);
 authRouter.post('/verifyLogin', authController.verifyLogin);
