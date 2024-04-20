31d218757e213b960f8a5b0fc7f266c97e26168f
Zhurbey Artem <zhurbeyartem@gmail.com>
2024-04-21 01:45:52 +0300

    add swagger

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 import mainRouter from "./routes/index.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger-output.json" assert { type: "json" };
 
 dotenv.config();
 const app = express();
@@ -14,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", mainRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((_, res) => {
     res.status(404).json({ message: "Route not found" });
@@ -25,6 +28,9 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+
+
 
 async function main() {
     try {
